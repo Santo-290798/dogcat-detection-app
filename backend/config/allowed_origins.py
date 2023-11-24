971e0dbcfe6ead6cf53724d0c9060674f5540579
doananhtingithub40102 <doananhtin2001st@gmail.com>
2023-11-24 09:04:18 +0700

    Add domain/resource allows making cross-origin requests (CORS)

--- backend/config/allowed_origins.py
+++ backend/config/allowed_origins.py
@@ -1,5 +1,6 @@
 # Allowed origins from client
 ALLOWED_ORIGINS = [
+    'https://dogcat-detection-app.vercel.app',
     'http://127.0.0.1:5173',
     'http://localhost:5173',
     'http://localhost:3000',
