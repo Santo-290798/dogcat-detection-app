aceb9ff832caecef0bbd70a3e8e6b9239935f87d
doananhtingithub40102 <doananhtin2001st@gmail.com>
2023-11-28 14:22:20 +0700

    Pull frontend part from main branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MyNavbar from './components/Navbar';
 import Main from './components/Main';
+import DescriptionPage from './components/DescriptionPage';
 import Footer from './components/Footer';
 
 export default function App() {
     return (
-        <>
+        <Router>
             <MyNavbar />
-            <Main
-                title="Dog&Cat Detection"
-                description="Detect Dogs and Cats on the image you have selected using AI"
-            />
+            <Routes>
+                <Route path="/" element={<Main title="Dog&Cat Detection" description="Detect Dogs and Cats on the image you have selected using AI" />} />
+                <Route path="/how-it-works" element={<DescriptionPage />} />
+            </Routes>
             <Footer />
-        </>
+        </Router>
     );
-}
+};
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -5,14 +6,25 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Logo from '../../img/logo.svg';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Link } from 'react-router-dom';
 import '../../css/main.css';
 import './style.css';
 
 export default function MyNavbar() {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleToggle = () => {
+        setExpanded(!expanded);
+    };
+
+    const handleSelect = () => {
+        setExpanded(false);
+    };
+
     return (
-        <Navbar expand="lg" fixed="top" className="shadow bg-white">
+        <Navbar expand="lg" fixed="top" className="shadow bg-white" expanded={expanded}>
             <Container>
-                <Navbar.Brand className="d-flex align-items-center" href="/">
+                <Link to="/" className="navbar-brand d-flex align-items-center">
                     <img
                         alt="logo"
                         src={Logo}
@@ -21,30 +33,31 @@ export default function MyNavbar() {
                         className="d-inline-block align-top"
                     />{' '}
                     <span className="brand"><span className="green-color">Dog&Cat</span>Detection</span>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                </Link>
+                <Navbar.Toggle aria-controls="offcanvasNavbar" onClick={handleToggle} />
                 <Navbar.Offcanvas
                     id="offcanvasNavbar"
                     aria-labelledby="offcanvasNavbarLabel"
                     placement="end"
+                    show={expanded}
                 >
-                    <Offcanvas.Header closeButton>
+                    <Offcanvas.Header closeButton onClick={handleToggle}>
                         <Offcanvas.Title id="offcanvasNavbarLabel">
                             Menu
                         </Offcanvas.Title>
                     </Offcanvas.Header>
                     <Offcanvas.Body>
                         <Nav className="justify-content-center flex-grow-1 menu-item">
-                            <Nav.Link href="#how-it-works">How it works</Nav.Link>
+                            <Link to="/how-it-works" className="nav-link" onClick={handleSelect}>How it works</Link>
                             <NavDropdown title="Solutions" id="solutions-dropdown">
-                                <NavDropdown.Item href="#solution/detect">Detection</NavDropdown.Item>
+                                <NavDropdown.Item href="/#solution/detect">Detection</NavDropdown.Item>
                                 {/* <NavDropdown.Item href="#solution/segment">Segmentation</NavDropdown.Item>
                                 <NavDropdown.Item href="#solution/classify">Classification</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#solution/pose">Pose estimation</NavDropdown.Item> */}
                             </NavDropdown>
                             <NavDropdown title="Features" id="features-dropdown">
-                                <NavDropdown.Item href="#feature/ai">Computer vision</NavDropdown.Item>
+                                <NavDropdown.Item href="/#feature/ai">Computer vision</NavDropdown.Item>
                                 {/* <NavDropdown.Item href="#feature/export">Export images</NavDropdown.Item> */}
                                 {/* <NavDropdown.Item href="#feature/auto-labeling">Auto labeling</NavDropdown.Item> */}
                             </NavDropdown>
