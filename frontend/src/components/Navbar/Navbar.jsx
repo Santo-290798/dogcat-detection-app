0ea2572fdc57204b94d199e8aac92ffa7d28bf95
doananhtingithub40102 <doananhtin2001st@gmail.com>
2023-11-28 09:23:14 +0700

    Update the Navbar component. It is  closed after clicking on a menu item

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,8 +11,18 @@ import '../../css/main.css';
 import './style.css';
 
 export default function MyNavbar() {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleToggle = () => {
+        setExpanded(!expanded);
+    };
+
+    const handleSelect = () => {
+        setExpanded(false);
+    };
+
     return (
-        <Navbar expand="lg" fixed="top" className="shadow bg-white">
+        <Navbar expand="lg" fixed="top" className="shadow bg-white" expanded={expanded}>
             <Container>
                 <Link to="/" className="navbar-brand d-flex align-items-center">
                     <img
@@ -23,20 +34,21 @@ export default function MyNavbar() {
                     />{' '}
                     <span className="brand"><span className="green-color">Dog&Cat</span>Detection</span>
                 </Link>
-                <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                <Navbar.Toggle aria-controls="offcanvasNavbar" onClick={handleToggle} />
                 <Navbar.Offcanvas
                     id="offcanvasNavbar"
                     aria-labelledby="offcanvasNavbarLabel"
                     placement="end"
+                    show={expanded}
                 >
-                    <Offcanvas.Header closeButton>
+                    <Offcanvas.Header closeButton onClick={handleToggle}>
                         <Offcanvas.Title id="offcanvasNavbarLabel">
                             Menu
                         </Offcanvas.Title>
                     </Offcanvas.Header>
                     <Offcanvas.Body>
                         <Nav className="justify-content-center flex-grow-1 menu-item">
-                            <Link to="/how-it-works" className="nav-link">How it works</Link>
+                            <Link to="/how-it-works" className="nav-link" onClick={handleSelect}>How it works</Link>
                             <NavDropdown title="Solutions" id="solutions-dropdown">
                                 <NavDropdown.Item href="/#solution/detect">Detection</NavDropdown.Item>
                                 {/* <NavDropdown.Item href="#solution/segment">Segmentation</NavDropdown.Item>
