aa21ff7765b5f7ef116b4ee82a0de07f45d638bc
doananhtingithub40102 <doananhtin2001st@gmail.com>
2024-07-28 12:01:43 +0700

    import and use the <Analytics/> and <SpeedInsights/> React components from Vercel packages into the app to able to track user interactions, monitor performance, etc.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { store, persistor } from './redux/store';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,6 +11,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Analytics />
+    <SpeedInsights />
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
