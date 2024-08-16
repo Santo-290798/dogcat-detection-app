7549e4f40f2eeb01b757252b38fda10d004de03c
doananhtingithub40102 <doananhtin2001st@gmail.com>
2024-08-16 14:17:41 +0700

    Add a new page 'Missing' to catch other missing routes - Page Not Found 404

--- frontend/src/_root/pages/Missing.jsx
+++ frontend/src/_root/pages/Missing.jsx
@@ -1,6 +1,23 @@
+import { Container, Row, Col, Image } from 'react-bootstrap';
+
 const Missing = () => {
   return (
-    <div>Missing</div>
+    <Container style={{ marginTop: '159px', marginBottom: '159px' }}>
+      <Row className='justify-content-center text-center'>
+        <Col lg={11}>
+          <div className='app-title highlight-color text-center'>Sorry, the page you're looking for can't be found.</div>
+          <a href='/' className='app-description main-text-hover d-inline-block mt-sm-5 mt-4'>
+            <Image
+              src='/assets/icons/left_arrow.svg'
+              alt='left arrow icon'
+              width={22}
+              className='left-arrow-icon me-2'
+            />
+            Go to the home page
+          </a>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
